ed55460718a47c87c20b408d3c19d4af5cd565f5
Faranaz08 <faranazannigeri3142@gmail.com>
2024-02-04 11:30:02 +0530

    Middlewares

--- FirstExpressProject/app.js
+++ FirstExpressProject/app.js
@@ -11,9 +11,11 @@ app.use((req,res,next)=>{
 
 app.use((req,res,next)=>{
     console.log("In the another middleware")
-    
+    res.send('<h1>Hello from Express</h1>')
+
 });
 
-const server=http.createServer(app);
+// const server=http.createServer(app);
 
-server.listen(4000);
+// server.listen(4000);
+app.listen(3000)
