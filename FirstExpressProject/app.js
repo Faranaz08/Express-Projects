1314894850b73436295413b12f7ff32647f1ed8f
Faranaz08 <faranazannigeri3142@gmail.com>
2024-02-04 12:29:48 +0530

    Body paser

--- FirstExpressProject/app.js
+++ FirstExpressProject/app.js
@@ -1,21 +1,21 @@
-const http=require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const express =require('express');
+const app = express();
 
-const app=express();
+app.use(bodyParser.urlencoded({extended: false}));
 
-app.use((req,res,next)=>{
-    console.log("In the middleware")
-    next();
+app.use('/add-product', (req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log("In the another middleware")
-    res.send('<h1>Hello from Express</h1>')
-
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-// const server=http.createServer(app);
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
 
-// server.listen(4000);
-app.listen(3000)
+app.listen(3000);
